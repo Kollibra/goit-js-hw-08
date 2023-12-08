ea3935ac6e4a239dcf05b347cbf55fa7f47fbadc
Iryna Kovalova <100217476+Kollibra@users.noreply.github.com>
2023-12-08 20:59:49 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,4 @@
-// const gallery = document.querySelector('.gallery');
+
 
 const images = [
   {
@@ -67,45 +67,6 @@ const images = [
   
 ];
 
-// const galleryContainer = document.querySelector('.gallery');
-// let lightboxInstance = null;
-
-// images.forEach(image => {
-//   const galleryItem = document.createElement('li');
-//   galleryItem.classList.add('gallery-item');
-
-//   const galleryLink = document.createElement('a');
-//   galleryLink.classList.add('gallery-link');
-//   galleryLink.href = image.original;
-
-//   const galleryImage = document.createElement('img');
-//   galleryImage.classList.add('gallery-image');
-//   galleryImage.src = image.preview;
-//   galleryImage.setAttribute('data-source', image.original);
-//   galleryImage.alt = image.description;
-
-//   galleryLink.appendChild(galleryImage);
-//   galleryItem.appendChild(galleryLink);
-//   galleryContainer.appendChild(galleryItem);
-
-//   galleryLink.addEventListener('click', event => {
-//     event.preventDefault(); 
-
-//     lightboxInstance = basicLightbox.create(
-//       `<img src="${image.original}" alt="${image.description}" style="max-width: 100%; height: auto;">`
-//     );
-//     lightboxInstance.show();
-
-//     document.addEventListener('keydown', handleKeyPress);
-//   });
-// });
-
-// function handleKeyPress(event) {
-//   if (event.key === 'Escape') {
-//     lightboxInstance.close();
-//     document.removeEventListener('keydown', handleKeyPress);
-//   }
-// }
 
 
 const galleryContainer = document.querySelector('.gallery');
@@ -145,7 +106,7 @@ function handleKeyPress(event) {
 
 galleryContainer.addEventListener('click', handleGalleryLinkClick);
 
-// Додавання зображень до галереї
+// Додавання зображень 
 images.forEach(image => {
   const galleryItem = document.createElement('li');
   galleryItem.classList.add('gallery-item');
